fd36d21716740b75f321e79d2052dcf6b70b0030
Seamus Quinn <sumaesquinn@gmail.com>
2018-05-09 17:53:55 -0600

    Display number of favorited cards

--- src/StatelessComponents/Controls/Controls.js
+++ src/StatelessComponents/Controls/Controls.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Controls = ({ favorites, setData }) => {
   return(
     <div>
-      <p>Favorites</p>
+      <p>Favorites: <span>{favorites.length}</span></p>
       <button onClick={setData}>PEOPLE</button>
       <button>PLANETS</button>
       <button>VEHICLES</button>
