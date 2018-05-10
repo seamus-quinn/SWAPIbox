a732481e8490fba8435cb5dd864f1bc899577220
Seamus Quinn <sumaesquinn@gmail.com>
2018-05-10 14:49:56 -0600

    Add fetch planets functionality

--- src/StatelessComponents/Controls/Controls.js
+++ src/StatelessComponents/Controls/Controls.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Controls = ({ favorites, setPeopleData, setVehicleData }) => {
+const Controls = ({ favorites, setPeopleData, setVehicleData, setPlanetData }) => {
   return(
     <div>
       <p>Favorites: <span>{favorites.length}</span></p>
       <button onClick={setPeopleData}>PEOPLE</button>
-      <button>PLANETS</button>
+      <button onClick={setPlanetData}>PLANETS</button>
       <button onClick={setVehicleData}>VEHICLES</button>
     </div>
   )
